98af3cfe2876c4c377269acbcc95dd763ec8ccf3
Daniel Martin <8dmartin4@gmail.com>
2022-06-03 07:36:33 -0700

    fixed Tilt related syntax error

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,11 +5,11 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="br2 shadow-2" style={{ height: 150, width: 150 }} >
+      <div className="br2 shadow-2" style={{ height: 150, width: 150 }} >
         <div className="pa3">
           <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
-      </Tilt>
+      </div>
     </div>
   );
 }
